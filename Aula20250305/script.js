21dc173f6808ae0ad42245d2c59acefa08684f35
LucasMeurerLeichtweis <lucas.leichtweis@aluno.feliz.ifrs.edu.br>
2025-03-12 15:46:49 -0300

    Atualizaçao
    Segue atualização

--- Aula20250305/script.js
+++ Aula20250305/script.js
@@ -1,11 +1,28 @@
-const botao = document.getElementById("botao");
-botao.style.backgroundColor = "red"
-const nome = document.getElementById("nome");
-nome.value = "Lucas"
+
 const formulario = document.getElementById("formulario")
 formulario.addEventListener("submit", function(event){
     event.preventDefault();
     alert("Você submeteu o formulário!")
 
+   const formData = new FormData(this);
+   let dados = Object.fromEntries(formData.entries());
+   console.log(dados);
+   /*
+   console.log(`${dados['nome']} tem ${dados['idade']} anos`)
+    */
+   
+    let texto = document.getElementById("texto");
+    //texto.textContent = `${dados['nome']} tem ${dados['idade']} anos`;//
+
+    if(formData.has("publico") && formData.has("privado")) {
+        texto.textContent =`${dados['nome']} vem de transporte público e privado`;
+    }else if(formData.has("publico")){
+        texto.textContent =`${dados['nome']} vem de transporte público`;
+    }else if(formData.has("privado")){
+        texto.textContent =`${dados['nome']} vem de transporte privado`;
+    }else{
+        texto.textContent =`${dados['nome']} não vem`;
+    }
+   
 });
 
